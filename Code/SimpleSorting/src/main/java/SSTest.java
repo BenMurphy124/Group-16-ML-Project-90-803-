7e1ecd2004c7775918dcb6ae7207bdbb22337437
Alex Jiang <alexjiang20232024@gmail.com>
2025-03-27 12:49:47 -0400

    initial commit

---
+++ Code/SimpleSorting/src/main/java/SSTest.java
@@ -0,0 +1,48 @@
+
+public class SSTest {
+
+
+    /**
+     * BubbleSort
+     * */
+    public static void bubbleSort(int[] data) {
+        for (int out = data.length - 1; out >= 1; out--) {
+//            当out = 1时，内层循环为0和1的比较，这也是最后一次比较，这次以后，0变成最小值
+//            当out = 0时， 内层循环只有0和0， 无需比较
+            for (int in = 0; in < out; in++) {
+                if (data[in+1] <data[in]) {
+                    swap(data, in+1, in);
+                }
+            }
+        }
+    }
+
+    /**
+     * SlectionSort
+     * */
+    public static void selectionSort(int[] data) {
+        for (int out = 0; out < data.length - 1; out++) {
+            int min = out;
+            for (int in = out + 1; in < data.length; in++) {
+                if (data[in] < data[min]) {
+                    min = in;
+                }
+            }
+            if (out != min) {
+                swap(data, out, min);
+            }
+        }
+    }
+
+
+
+
+    /**
+     * swap method swap the element in index a with element in index b.
+     * */
+    public static void swap(int[] data, int a, int b){
+            int temp = data[a];
+            data[a] = data[b];
+            data[b] = temp;
+        }
+}
